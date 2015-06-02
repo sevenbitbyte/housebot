4fb8cd1ec4c44ae8c316a42571b96e42fc5ca80f
Alan Meekins <alan.meekins@gmail.com>
2015-06-01 23:54:01 -0700

    Install script updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,12 @@
 PATH=$(dirname $(readlink -f $0))
 PKGDIR=${PATH%/*}
 
+. ~/.bashrc
+
+
 echo $PKGDIR
 roscd
 cd ..
 
-. ~/.bashrc
 catkin_make housebot_display_firmware
 catkin_make housebot_display_firmware_display-upload
