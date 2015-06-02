3faf8be30d32d77fbd8209b9494b5f0e947a25d1
Alan Meekins <alan.meekins@gmail.com>
2015-06-01 23:26:52 -0700

    Script updates

--- scripts/installMetal.sh
+++ scripts/installMetal.sh
@@ -14,8 +14,9 @@ tar -cf $STAGING_DIR.$PACKAGE.tar $STAGING_DIR
 
 bzip2 -z $STAGING_DIR.$PACKAGE.tar
 
+ssh $SSH_URL rm -rf /home/vega/Repos/$PACKAGE*
 scp $STAGING_DIR.$PACKAGE.tar.bz2 $SSH_URL:/home/vega/Repos/$PACKAGE.tar.bz2
 ssh $SSH_URL bzip2 -d /home/vega/Repos/$PACKAGE.tar.bz2
 ssh $SSH_URL tar -xf /home/vega/Repos/$PACKAGE.tar
 ssh $SSH_URL bash /home/vega/Repos/$PACKAGE/scripts/setup.sh
-ssh $SSH_URL rm /home/vega/Repos/$PACKAGE.tar.bz2
+ssh $SSH_URL rm /home/vega/Repos/$PACKAGE.tar*
