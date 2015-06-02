e6f92b5ffa997ef9278d63a7ca73777a5da118cf
Alan Meekins <alan.meekins@gmail.com>
2015-06-01 23:23:34 -0700

    Installation scripts

---
+++ scripts/installMetal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PACKAGE=${PWD##*/}
+STAGING_DIR=$(mktemp)
+REMOTE_USER=vega
+REMOTE_HOST=192.168.8.1
+SSH_URL=$REMOTE_USER@$REMOTE_HOST
+
+rm -rf $STAGING_DIR
+
+git clone ./ $STAGING_DIR
+
+tar -cf $STAGING_DIR.$PACKAGE.tar $STAGING_DIR
+
+bzip2 -z $STAGING_DIR.$PACKAGE.tar
+
+scp $STAGING_DIR.$PACKAGE.tar.bz2 $SSH_URL:/home/vega/Repos/$PACKAGE.tar.bz2
+ssh $SSH_URL bzip2 -d /home/vega/Repos/$PACKAGE.tar.bz2
+ssh $SSH_URL tar -xf /home/vega/Repos/$PACKAGE.tar
+ssh $SSH_URL bash /home/vega/Repos/$PACKAGE/scripts/setup.sh
+ssh $SSH_URL rm /home/vega/Repos/$PACKAGE.tar.bz2
