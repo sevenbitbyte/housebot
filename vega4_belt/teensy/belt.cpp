211e129f186889c784ef02b533f5142249597600
Alan Meekins <alan.meekins@gmail.com>
2016-05-16 02:26:19 -0700

    Belt RGBW initial support

--- vega4_belt/teensy/belt.cpp
+++ vega4_belt/teensy/belt.cpp
@@ -1,7 +1,10 @@
 #include "Arduino.h"
 
+#include <Adafruit_NeoPixel.h>
+
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int8MultiArray.h>
 
 const int ledPin = 13;
 const int vMonPin = 14;
@@ -10,7 +13,9 @@ const int powerEAPin = 2;
 
 #define V_MON_COEF (16.35f / 658)
 
-ros::NodeHandle_<ArduinoHardware, 25, 25, 2048, 2048> nh;
+Adafruit_NeoPixel beltPixels = Adafruit_NeoPixel(23, 20, NEO_RGBW + NEO_KHZ800);
+
+ros::NodeHandle_<ArduinoHardware, 25, 25, 4096, 4096> nh;
 
 std_msgs::Float32 voltageMsg;
 std_msgs::Float32 currentMsg;
@@ -18,23 +23,56 @@ std_msgs::Float32 currentMsg;
 ros::Publisher voltagePub("voltage", &voltageMsg);
 ros::Publisher currentPub("current", &currentMsg);
 
+void setStripMultiArray(Adafruit_NeoPixel& strip, const std_msgs::Int8MultiArray& pixels){
+  for(int i=0; i<strip.numPixels() && i<pixels.data_length; i++){
+    int idx = i*4;	//NOTE: We expect RGBW data only
+
+    byte red = pixels.data[idx];
+    byte green = pixels.data[idx + 1];
+    byte blue = pixels.data[idx + 2];
+    byte white = pixels.data[idx + 3];
+
+    strip.setPixelColor(i, strip.Color( red, green, blue, white));
+  }
+
+  strip.show();
+}
+
+void beltCb(const std_msgs::Int8MultiArray& pixels){
+  setStripMultiArray(beltPixels, pixels);
+}
+
+void setStrip(Adafruit_NeoPixel* strip, int red, int green, int blue, int white, int gamma) {
+
+  for (int x = 0; x < strip->numPixels(); x++ ) {
+    strip->setPixelColor(x, red, green, blue, white);
+  }
+
+  strip->show();
+}
+
+ros::Subscriber<std_msgs::Int8MultiArray> beltSub("/belt/belt_led_rgbw", &beltCb );
+
 void setup() {
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
   pinMode(powerEAPin, OUTPUT);
   digitalWrite(powerEAPin, LOW);
+
+  beltPixels.setBrightness(50);
+  beltPixels.begin();
+  setStrip(&beltPixels, 0, 0 ,0, 0, 50);
 
   nh.initNode();
   nh.getHardware()->setBaud(115200);
   nh.advertise(voltagePub);
   nh.advertise(currentPub);
+  nh.subscribe(beltSub);
 }
 
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-  delay(100);
 
   voltageMsg.data = analogRead(vMonPin) * V_MON_COEF;
   voltagePub.publish(&voltageMsg);
@@ -51,5 +89,16 @@ void loop() {
 
   nh.spinOnce();
 
+  if(nh.connected()){
+
+  }
+  else{
+    float i = (((float)(millis()%3500)) / 3500) * M_PI;
+    uint8_t x = (sin(i) * 256.0) + 0.0;
+
+    beltPixels.setPixelColor(0, x, x, x, x);
+
+    beltPixels.show();
+  }
 }
 
